b1f7f464f448f5e17233b1f4aaa0d3a71cb60d53
william <william@magicmanager.nl>
2022-09-10 22:02:23 +0200

    Update user

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -113,6 +113,9 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['data' => $user], 200);
     }
 }
