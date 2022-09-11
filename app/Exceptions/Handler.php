6c4bb393f3459a5d02e3fe321e6bc8df6abafbae
william <william@magicmanager.nl>
2022-09-11 15:58:56 +0200

    Returning MethodNotAllowedException, HttpException, QueryException, UnexpectedException validation errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,9 +6,12 @@ use App\Traits\ApiResponser;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 use Throwable;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
@@ -77,8 +80,26 @@ class Handler extends ExceptionHandler
         if ($e instanceof NotFoundHttpException) {
             return $this->errorResponse('The specified url can not be found.', 404);
         }
+        if ($e instanceof MethodNotAllowedException) {
+            return $this->errorResponse('The specified method for the request is invalid.', 405);
+        }
+        if ($e instanceof HttpException) {
+            return $this->errorResponse($e->getMessage(), $e->getStatusCode());
+        }
+        if ($e instanceof QueryException) {
+            $errorCode = $e->errorInfo[1];
+            if ($errorCode === 1451) {
+                return $this->errorResponse(
+                    'Can not remove this resource permanently. It is related with any other resource.',
+                    409
+                );
+            }
+        }
+        if (config('app.debug')) {
+            return parent::render($request, $e);
+        }
 
-        return parent::render($request, $e);
+        return $this->errorResponse('Unexpected Exception. Try Later', 500);
     }
 
     protected function unauthenticated($request, AuthenticationException $exception): JsonResponse
