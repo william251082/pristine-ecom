db8be691fcd9487b7fd961d24e1a1235a0a74fef
william <william@magicmanager.nl>
2022-09-17 14:58:36 +0200

    Implemented TransformInput

--- app/Http/Middleware/TransformInput.php
+++ app/Http/Middleware/TransformInput.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+use League\Fractal\TransformerAbstract;
+
 class TransformInput
 {
+    public function handle(Request $request, Closure $next, TransformerAbstract|string $transformer)
+    {
+        $transformedInput = [];
 
+        foreach ($request->request->all() as $input => $value) {
+            $transformedInput[$transformer::originalAttribute($input)] = $value;
+        }
+        $request->replace($transformedInput);
+
+        return $next($request);
+    }
 }
